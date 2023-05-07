1f065bdccc14eaf094c8e9bf6e1a16beeec3ce55
snakagomi <tanakagoro0319@yahoo.ne.jp>
2023-05-07 15:44:35 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+extern crate minigrep2;
+
+use std::env;
+use std::process;
+
+use minigrep2::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep2::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    };
+}
+
+
+
+
